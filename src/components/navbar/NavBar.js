81eead5aa46a4aa8732ba0eba9be6b05a65f5117
MathiasVM <mathiasvidarte.m@gmail.com>
2023-04-04 20:44:40 -0300

    creacion navbar

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>MVsports</h3>
+            <div>
+                <button>Ropa</button>
+                <button>Calzado</button>
+                <button>Accsesorios</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
