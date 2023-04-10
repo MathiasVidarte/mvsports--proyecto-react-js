6de4911b60937ba3c9e33b4f7584fafd7141ee9b
MathiasVM <mathiasvidarte.m@gmail.com>
2023-04-09 21:47:07 -0300

    Agregue navbar boostrap

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,16 +1,28 @@
 import CartWidget from "./CartWidget/CartWidget"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const NavBar = () => {
     return (
-        <nav>
-            <h3>MVsports</h3>
-            <div>
-                <button>Ropa</button>
-                <button>Calzado</button>
-                <button>Accesorios</button>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">MVsports</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link" href="#">Vestimenta</a>
+              <a class="nav-link" href="#">Calzado</a>
+              <a class="nav-link" href="#">Accesorios</a>
+              <a class="nav-link" href="#">Sale</a>
             </div>
-            <CartWidget/>
-        </nav>
+          </div>
+        </div>
+      
+        
+        <CartWidget/>
+     </nav>
     )
 }
 
