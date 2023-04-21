26a60a46d425174dcacba2415ba26f80b8004430
MathiasVM <mathiasvidarte.m@gmail.com>
2023-04-20 22:15:11 -0300

    cree ItemCount

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
     <div className="App">
      <NavBar />
      <ItemListContainer greeting= {'Bienvenidos a nuestra tienda on-line'} />
-     
+     <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('Cantidad agregada', quantity)}/>
     </div>
 
   );
