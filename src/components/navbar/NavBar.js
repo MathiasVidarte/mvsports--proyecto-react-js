89e6eb88d80b18fb0573ba2c66aacf53d3c00bff
MathiasVM <mathiasvidarte.m@gmail.com>
2023-04-25 22:15:36 -0300

    arreglos css

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -8,7 +8,9 @@ const NavBar = () => {
       return (
         <nav className="mi-navegacion navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">MVsports</a>
+            <h3>
+            <NavLink exact to={`/`} className="navbar-brand">MVsports</NavLink>
+            </h3>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
