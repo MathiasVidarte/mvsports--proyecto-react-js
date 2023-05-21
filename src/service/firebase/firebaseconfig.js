185f3b2330908c74ffae55ad8b8ea329e92ddd5f
MathiasVM <mathiasvidarte.m@gmail.com>
2023-05-21 19:51:03 -0300

    variables de entorno

--- src/service/firebase/firebaseconfig.js
+++ src/service/firebase/firebaseconfig.js
@@ -1,20 +1,18 @@
-// Importa las funciones que necesitas de los SDK que necesitas
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
-// Tu configuración de Firebase para la aplicación web
+
 const firebaseConfig = {
-    apiKey: "AIzaSyB_Ggjuhqmx3kHZmfMVgPX97LLuUniWi0M",
-    authDomain: "mvsport-34228.firebaseapp.com",
-    projectId: "mvsport-34228",
-    storageBucket: "mvsport-34228.appspot.com",
-    messagingSenderId: "242949404239",
-    appId: "1:242949404239:web:f7c6fdf6816de2fc1b1c3a"
+    apiKey: process.env.REACT_APP_apikey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
-// Inicializa Firebase
-const app = initializeApp(firebaseConfig);
 
-// Obtiene una instancia de Firestore
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
