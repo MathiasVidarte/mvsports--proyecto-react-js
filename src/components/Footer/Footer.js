d1343f58d51a7594cb50e9ac3e63ae913d1ef321
MathiasVM <mathiasvidarte.m@gmail.com>
2023-05-26 23:09:34 -0300

    arreglos codigo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,24 +1,20 @@
 import { FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa';
 import './Footer.css'; 
+
 function Footer() {
   return (
     <div className="footer">
-      <div>
       <div className="social-icons">
-         <link to="https://twitter.com"><FaTwitter/></link>
-        <link to="https://facebook.com"><FaFacebook /></link>
-        <link to="https://instagram.com"><FaInstagram /></link>
-       </div>
-       <div className='links'>
+        <a href="https://twitter.com"><FaTwitter/></a>
+        <a href="https://facebook.com"><FaFacebook /></a>
+        <a href="https://instagram.com"><FaInstagram /></a>
+      </div>
+      <div className='links'>
         <p>Nosotros</p>
         <p>Locales</p>
         <p>Contacto</p>
-        </div>
-       
-       </div>
+      </div>
       <p>© 2023 MVsports, Inc. Todos los derechos reservados.</p>
-    
-     
     </div>
   );
 }
